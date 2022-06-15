26e07beefbed437e7285c2e3e3d301f90b46dbe2
Jose <123layrton@gmai.com>
2022-06-15 10:03:55 -0300

    First commit

---
+++ src/main/java/com/stefanini/hackathon/repository/AlunoRepository.java
@@ -0,0 +1,10 @@
+package com.stefanini.hackathon.repository;
+
+import com.stefanini.hackathon.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+
+
+
+}
